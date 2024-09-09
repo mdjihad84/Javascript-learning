5e6a2e06d33ffe11f0657d9495a332eb2537830e
MD JAHIDUL ISLAM JIHAD <mdj847621@gmail.com>
2024-09-09 11:09:20 +0600

    javascript variables complete

---
+++ app.js
@@ -0,0 +1,109 @@
+                /*
+                * 1. what is js?
+                * 2. declare korba variables.
+                * 3. output dekhaiba variables.
+                * 4.ki ki data type ache ar kondar ki kaj eidao leikka aynno
+                */
+
+                                    // What is javascript
+
+              /*
+                *.1 JavaScript is a scripting language that enables you to create dynamically 
+                * updating content, control multimedia, animate images, and pretty much 
+                * everything else. 
+              */           
+        
+        
+        
+                                 // Javascript variables
+                    2.
+                    /*
+                  * JavaScript variable names are case-sensitive
+                    ! Variable names cannot contain spaces.
+                    * Variables cannot be the same as reserved keywords such as `if` or `const`.
+                    * By convention, JavaScript variable names are written in camelCase.
+                    * Variables should be given descriptive names that indicate their content and usage (e.g. `sellingPrice` and `costPrice` rather than `x` and `y`).
+                    * You can not add number first
+                    * _, $ is acceptable but - not accaptable
+                    */
+                        
+                    /*
+                                                    ? Var Variable         
+                    * You can re declare
+                    * You can re assaign
+                    * Global Scooped
+                    * var is hosted
+                    */
+                    // var: The old way, not recommended for modern JavaScript.
+
+                    var contry = 'bangladesh was a small country';
+                    console.log(contry);
+
+                    var a = 25;
+                    var b = 45;
+                    var sum = a + b;
+                    console.log(sum);
+
+                    var p = 120;
+                    var q = 20;
+                    var sum = p - q;
+                    console.log(sum);
+
+                    var p = 80;
+                    var q = 30;
+                    var sum = p * q;
+                    console.log(sum);
+                    
+                    var p = 150;
+                    var q = 10;
+                    var sum = p / q;
+                    console.log(sum); 
+
+                    
+                  /*
+                                                    ? let Variable
+                    ! You can not re declare
+                    * You can re assaign
+                    ? Block Scooped
+                    ! let is not hosted
+                  */
+                    
+                    let nam = "JIHAD";
+                    let greeting = "Happy birthday " + nam;
+                    console.log(greeting);
+                    
+                    var p = 150;
+                    var q = 10;
+                    var sum = p * q;
+                    console.log(sum); 
+                    
+                        
+                    /*
+                                                    ? const Variable
+                    ! You can not re declare
+                    ! You can not re assaign
+                    ? Block Scooped
+                    ! const is not hosted
+                    */
+                        
+                                                  // javascript data type
+                    3. /*
+                    ? JavaScript has 8 Datatypes
+                    1. String
+                    2. Number
+                    3. Bigint
+                    4. Boolean
+                    5. Undefined
+                    6. Null
+                    7. Symbol
+                    8. Object
+                    */   
+                        
+                    3. /*
+                                                      ? BigInt
+                        
+                     *To create a BigInt, append n to the end of an integer or call BigInt()
+                     * BigInt can not mix with other data types
+                     * A BigInt can not have decimals
+                     */
+                     
